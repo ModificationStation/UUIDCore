a39b218271f629b3c76b17835b7554342c5e8140
Johny Muffin <jetpackingwolf@gmail.com>
2019-12-24 18:15:25 +1000

    Fix Event

--- src/com/johnymuffin/uuidcore/event/PlayerUUIDEvent.java
+++ src/com/johnymuffin/uuidcore/event/PlayerUUIDEvent.java
@@ -1,4 +1,39 @@
 package com.johnymuffin.uuidcore.event;
 
-public class PlayerUUIDEvent {
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+
+import java.util.UUID;
+
+public class PlayerUUIDEvent extends Event implements Cancellable {
+    private boolean isCancelled = false;
+    private Player player;
+    private java.util.UUID UUID;
+    public PlayerUUIDEvent(Player p, UUID u) {
+        super("UUIDCoreLoginEvent");
+        UUID = u;
+        player = p;
+
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return isCancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean b) {
+        isCancelled = b;
+
+    }
+
+    public Player getPlayer() {
+        return player;
+    }
+
+    public UUID getPlayerUUID() {
+        return UUID;
+    }
+
 }
--- src/com/johnymuffin/uuidcore/event/callPlayerUUIDEvent.java
+++ src/com/johnymuffin/uuidcore/event/callPlayerUUIDEvent.java
@@ -1,5 +1,6 @@
 package com.johnymuffin.uuidcore.event;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 import java.util.UUID;
@@ -7,43 +8,11 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 
-class PlayerUUIDEvent extends Event implements Cancellable{
-    private boolean isCancelled = false;
-    private Player player;
-    private UUID UUID;
-    public PlayerUUIDEvent(Player p, UUID u) {
-        super("UUIDCoreLoginEvent");
-        UUID = u;
-        player = p;
-
-    }
-
-    @Override
-    public boolean isCancelled() {
-        return isCancelled;
-    }
-
-    @Override
-    public void setCancelled(boolean b) {
-        isCancelled = b;
-
-    }
-
-    public Player getPlayer() {
-        return player;
-    }
-
-    public UUID getPlayerUUID() {
-        return UUID;
-    }
-
-}
-
 public class callPlayerUUIDEvent {
 
     public static void callUUIDLogin(Player p, UUID u) {
         final PlayerUUIDEvent event = new PlayerUUIDEvent(p, u);
-
+        Bukkit.getServer().getPluginManager().callEvent(event);
     }
 
 
